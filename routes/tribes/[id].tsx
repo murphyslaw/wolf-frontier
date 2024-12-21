cf74b827158ded6f163a8fe649d00f2bcfc0f9bf
Falk Köppe <falk.koeppe@gmail.com>
2024-12-21 21:52:44 +0100

    add killmails to tribe details page

--- routes/tribes/[id].tsx
+++ routes/tribes/[id].tsx
@@ -1,6 +1,8 @@
 import { FreshContext } from "$fresh/server.ts";
+import { Killmails } from "../../components/Killmails.tsx";
 import { Tribe } from "../../components/Tribe.tsx";
 import { characterService } from "../../utils/CharacterService.ts";
+import { killmailService } from "../../utils/KillmailService.ts";
 import { tribeService } from "../../utils/TribeService.ts";
 
 export default async function TribeDetailsPage(
@@ -10,10 +12,19 @@ export default async function TribeDetailsPage(
   const tribeId = Number(ctx.params.id);
   const tribe = await tribeService.get(tribeId);
   const members = await characterService.listByTribe(tribeId);
+  const killmails = await killmailService.findByTribe(tribeId);
 
   if (!tribe) {
     return ctx.renderNotFound();
   }
 
-  return <Tribe tribe={tribe} members={members} />;
+  return (
+    <>
+      <Tribe tribe={tribe} members={members} />
+
+      <section class="flex flex-col gap-y-8 items-center">
+        <Killmails killmails={killmails} />
+      </section>
+    </>
+  );
 }
